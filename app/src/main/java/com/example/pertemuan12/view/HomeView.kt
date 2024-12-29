78f4ac298a845fd488297809b77fccc4e9b62a5e
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 22:00:07 +0700

    membuat fungsi HomeScreen

--- app/src/main/java/com/example/pertemuan12/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/view/HomeView.kt
@@ -49,6 +49,50 @@ object DestinasiHome: DestinasiNavigasi{
     override val titleRes = "Home mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry:() ->Unit,
+    modifier: Modifier =Modifier,
+    onDetailClik:(String)-> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior =TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CostumTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Mahasiswa")
+            }
+        },
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUiState,
+            retryAction = {viewModel.getMhs()},modifier = Modifier.padding(innerPadding),
+            onDetilClick = onDetailClik, onDeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
+
+
 @Composable
 fun HomeStatus(
     homeUiState: HomeUiState,
