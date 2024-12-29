76e2358dde7db553b03f6374ba0fe64d937e8024
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 15:33:20 +0700

    menambahkan data class untuk InsertUiEvent

--- app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.pertemuan12.viewmodel
 
-import android.provider.ContactsContract.Intents.Insert
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,14 +10,14 @@ import com.example.pertemuan12.Repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
 class InsertViewModel (
-    private val  mhs:MahasiswaRepository
+    private val  mhs: MahasiswaRepository
 ): ViewModel(){
 
     var uiState by mutableStateOf(InsertUiState())
 
         private set
 
-    fun updateInsertMhsState(inserUiEvent.InsertUiEvent){
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
         uiState = InsertUiState(insertUiEvent = insertUiEvent)
     }
 
@@ -26,16 +25,38 @@ class InsertViewModel (
         viewModelScope.launch {
             try {
                 mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
             }
         }
     }
 }
 
+data class  InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+data class InsertUiEvent(
+    val nim:String = "",
+    val nama: String="",
+    val alamat: String="",
+    val jenisKelamin : String = "",
+    val kelas: String = "",
+    val angkatan : String = ""
+)
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
 
-fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     nim = nim,
     nama = nama,
     alamat = alamat,
