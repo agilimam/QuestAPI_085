1e46cb4ad295bf359a351665c8fa9f000c2b9b15
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 13:55:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan12/Service/MahasiswaService.kt
@@ -0,0 +1,33 @@
+package com.example.pertemuan12.Service
+
+import com.example.pertemuan12.Mahasiswa.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa() : List<Mahasiswa>
+
+    @GET("baca1mahaswia.php/{nim}")
+    suspend fun  getMahasiswaByNim(@Query("nim") nim:String):Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiwa: Mahasiswa)
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim") nim: String, @Body mahasiwa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim") nim: String) : Response<Void>
+}
