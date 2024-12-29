5ff6d70de91110988f563b7d74d2d09e62b78579
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 15:01:20 +0700

    menambahkan fungsi untuk mengubah Mahasiswa ke InsertUiEvent

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.Mahasiswa.Mahasiswa
+import com.example.pertemuan12.Repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val  mhs:MahasiswaRepository
+): ViewModel(){
+
+    var uiState by mutableStateOf(InsertUiState())
+
+        private set
+
+    fun updateInsertMhsState(inserUiEvent.InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }
+        }
+    }
+}
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan=angkatan,
+)
