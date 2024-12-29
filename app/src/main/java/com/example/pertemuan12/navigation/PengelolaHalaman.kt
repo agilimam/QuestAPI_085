c4acd68acb695f2538a3efb8d253590488ac5318
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 21:15:38 +0700

    menambahkan navigasi dengan NavHost untuk HomeScreen dan EntryMhsScreen

---
+++ app/src/main/java/com/example/pertemuan12/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.pertemuan12.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.view.DestinasiEntry
+import com.example.pertemuan12.view.DestinasiHome
+import com.example.pertemuan12.view.EntryMhsScreen
+import com.example.pertemuan12.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClik = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive =true
+                    }
+                }
+            })
+        }
+    }
+}
