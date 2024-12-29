12faf6059ca5ff9437c96c43b0daeb91740849ce
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 16:31:16 +0700

    Menambahkan CostumTopAppBar untuk membuat tampilan app bar kustom

---
+++ app/src/main/java/com/example/pertemuan12/CostumWidget/TopAppBar.kt
@@ -0,0 +1,20 @@
+package com.example.pertemuan12.CostumWidget
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumTopAppBar(
+    title : String,
+    canNavigateBack:Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp:() ->Unit = {},
+    onRefresh: ()->Unit= {},
+){
+
+}
