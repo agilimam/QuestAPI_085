920e6a66740da07fb4368e9be610080d6b995177
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 16:56:53 +0700

    Menambahkan EntryMhsScreen dengan top bar kustom dan integrasi dengan ViewModel

--- app/src/main/java/com/example/pertemuan12/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/view/InsertView.kt
@@ -1,9 +1,47 @@
 package com.example.pertemuan12.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.CostumWidget.CostumTopAppBar
 import com.example.pertemuan12.navigation.DestinasiNavigasi
+import com.example.pertemuan12.viewmodel.InsertViewModel
+import com.example.pertemuan12.viewmodel.PenyediaViewModel
 
 object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun EntryMhsScreen(
+    navigateBack:() -> Unit,
+    modifier: Modifier =Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory )
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+
+
+    }
+
+}
