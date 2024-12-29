f4e22400cf47d4057c89a091b1a78ef86180840d
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 17:20:34 +0700

    menambahkan Composable EntryBody untuk layout form input data mahasiswa

--- app/src/main/java/com/example/pertemuan12/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/view/InsertView.kt
@@ -1,5 +1,8 @@
 package com.example.pertemuan12.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -7,9 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.CostumWidget.CostumTopAppBar
 import com.example.pertemuan12.navigation.DestinasiNavigasi
+import com.example.pertemuan12.viewmodel.InsertUiEvent
+import com.example.pertemuan12.viewmodel.InsertUiState
 import com.example.pertemuan12.viewmodel.InsertViewModel
 import com.example.pertemuan12.viewmodel.PenyediaViewModel
 
@@ -39,9 +45,25 @@ fun EntryMhsScreen(
                 navigateUp = navigateBack
             )
         }
-    ){
+    ){ innerPadding ->
+
 
 
     }
 
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier =Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+
+    }
 }
