b5a09b9f55dd56c97e44bbacc3df3be8d772d088
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 17:47:33 +0700

    menambahkan object destinasihome

---
+++ app/src/main/java/com/example/pertemuan12/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan12.view
+
+import com.example.pertemuan12.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home mhs"
+}
