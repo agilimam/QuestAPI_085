e33716799fe2033506c12416904e6228d422cec2
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 16:36:58 +0700

    implementasi CenterAlignedTopAppBar dengan tombol refresh

--- app/src/main/java/com/example/pertemuan12/CostumWidget/TopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/CostumWidget/TopAppBar.kt
@@ -1,6 +1,12 @@
 package com.example.pertemuan12.CostumWidget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,5 +22,14 @@ fun CostumTopAppBar(
     navigateUp:() ->Unit = {},
     onRefresh: ()->Unit= {},
 ){
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "",
+                modifier = Modifier.clickable {
+                    onRefresh()
+                })
+        }
+    )
 
 }
