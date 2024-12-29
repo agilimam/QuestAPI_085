1f74cde8a649c71b83753515645cd083c49766da
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 16:41:02 +0700

    membuat objek DestinasiEntry dan mendifinisikan route

---
+++ app/src/main/java/com/example/pertemuan12/view/InsertView.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan12.view
+
+import com.example.pertemuan12.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
