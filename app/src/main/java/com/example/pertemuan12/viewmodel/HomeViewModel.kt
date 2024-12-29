669173213144eadad73737d8a363759d700bcc68
agilimam <agilimamalhafizi04@gmail.com>
2024-12-29 15:43:29 +0700

    menambahkan HomeViewModel untuk mengelola status UI mahasiswa"

--- app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.pertemuan12.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.pertemuan12.Mahasiswa.Mahasiswa
+import com.example.pertemuan12.Repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 sealed class HomeUiState{
     data class Succes(val mahasiswa : List<Mahasiswa>) : HomeUiState()
@@ -8,4 +16,26 @@ sealed class HomeUiState{
     object Loading : HomeUiState()
 }
 
+class HomeViewModel (
+    private val mhs:MahasiswaRepository
+) :ViewModel(){
+    var mhsUiState:HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
 
+
+    init {
+        getMhs()
+    }
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Succes(mhs.getMahasiswa())
+            } catch (e:Exception){
+                HomeUiState.Error
+            } catch (e:HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+}
