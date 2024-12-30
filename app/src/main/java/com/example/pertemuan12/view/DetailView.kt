bc816e7a31631759e3c727412ef30f878e45a578
agilimam <agilimamalhafizi04@gmail.com>
2024-12-30 10:53:36 +0700

    Menambahkan fungsi DetailUi untuk menampilkan UI detail mahasiswa

--- app/src/main/java/com/example/pertemuan12/view/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12/view/DetailView.kt
@@ -5,9 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,15 +22,59 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.CostumWidget.CostumTopAppBar
 import com.example.pertemuan12.Mahasiswa.Mahasiswa
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 import com.example.pertemuan12.viewmodel.DetailUiState
+import com.example.pertemuan12.viewmodel.DetailViewModel
+import com.example.pertemuan12.viewmodel.PenyediaViewModel
 
 object DestinasiDetail: DestinasiNavigasi{
     override val route = "detail"
     override val titleRes ="Detail Mhs"
     const val Nim = "nim"
     val routesWithArg = "$route/{${Nim}im}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailUi(
+    navigateBack: () -> Unit,
+    navigateToitemUpdate: ()->Unit,
+    modifier: Modifier =Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold (
+        topBar = {
+            CostumTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getMahasiswabyNim()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToitemUpdate,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ){ innerPadding ->
+        DetailStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.mahasiswaDetailState,
+            retryAction = {viewModel.getMahasiswabyNim() }
+        )
+    }
 }
 
 @Composable
