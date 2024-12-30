7824f561fdb3c6621c29dafc3744cc5bb79c75e3
agilimam <agilimamalhafizi04@gmail.com>
2024-12-30 12:08:53 +0700

    membuat Fungsi UpdateScreen

--- app/src/main/java/com/example/pertemuan12/view/UpdateView.kt
+++ app/src/main/java/com/example/pertemuan12/view/UpdateView.kt
@@ -1,10 +1,64 @@
 package com.example.pertemuan12.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.CostumWidget.CostumTopAppBar
 import com.example.pertemuan12.navigation.DestinasiNavigasi
+import com.example.pertemuan12.viewmodel.PenyediaViewModel
+import com.example.pertemuan12.viewmodel.UpdateViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 object DestinasiUpdate: DestinasiNavigasi {
     override val route = "update"
     override val titleRes = "Update Mhs"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate: () -> Unit,
+    viewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        topBar = {
+            CostumTopAppBar(
+                title = DestinasiUpdate.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack
+            )
+        }
+    ){innerPadding ->
+        EntryBody(
+            modifier = Modifier.padding(innerPadding),
+            insertUiState = viewModel.updateUiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMhs()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate
+                    }
+                }
+            }
+        )
+    }
 }
