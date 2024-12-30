8ef4a4c5f38decad0e52076f15a4776e0b34d084
agilimam <agilimamalhafizi04@gmail.com>
2024-12-30 10:27:15 +0700

    Menambahkan fungsi itemDetailMhs untuk menampilkan detail mahasiswa

--- app/src/main/java/com/example/pertemuan12/view/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12/view/DetailView.kt
@@ -2,13 +2,19 @@ package com.example.pertemuan12.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pertemuan12.Mahasiswa.Mahasiswa
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi{
@@ -18,6 +24,31 @@ object DestinasiDetail: DestinasiNavigasi{
     val routesWithArg = "$route/{${Nim}im}"
 }
 
+@Composable
+fun itemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+){
+    Card (
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
+
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            DetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            DetailMhs(judul = "NAMA", isinya = mahasiswa.nama)
+            DetailMhs(judul = "JenisKelamin", isinya = mahasiswa.jenisKelamin)
+            DetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            DetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            DetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+
+    }
+
+}
 
 @Composable
 fun DetailMhs(
