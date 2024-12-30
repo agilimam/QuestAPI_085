a84743e73eb09ecd9283def411cc67876bce8858
agilimam <agilimamalhafizi04@gmail.com>
2024-12-30 10:43:30 +0700

    membuat fungsi untuk pengambilan data mahasiswa berdasarkan Nim

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -5,9 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.pertemuan12.Mahasiswa.Mahasiswa
 import com.example.pertemuan12.Repository.MahasiswaRepository
 import com.example.pertemuan12.view.DestinasiDetail
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class DetailUiState{
     data class Succes(val mahasiswa: Mahasiswa) : DetailUiState()
@@ -24,4 +28,24 @@ class DetailViewModel (
 
     private val _nim :String = checkNotNull(savedStateHandle[DestinasiDetail.Nim])
 
+    init {
+        getMahasiswabyNim()
+
+    }
+
+    fun getMahasiswabyNim(){
+        viewModelScope.launch {
+            mahasiswaDetailState = DetailUiState.Loading
+            mahasiswaDetailState = try {
+                val mahasiswa = Mhs.getMahasiswaByNim(_nim)
+                DetailUiState.Succes(mahasiswa)
+            }catch (e: IOException){
+                DetailUiState.Error
+            }catch (e: HttpException){
+                DetailUiState.Error
+            }
+        }
+    }
 }
+
+
