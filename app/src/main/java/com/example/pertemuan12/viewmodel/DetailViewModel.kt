85c7e55a3ba97f6451510f486338d4ce8aeaaf1f
agilimam <agilimamalhafizi04@gmail.com>
2024-12-30 10:38:02 +0700

    Menambahkan DetailViewModel untuk mengelola state

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.Mahasiswa.Mahasiswa
+import com.example.pertemuan12.Repository.MahasiswaRepository
+import com.example.pertemuan12.view.DestinasiDetail
+
+sealed class DetailUiState{
+    data class Succes(val mahasiswa: Mahasiswa) : DetailUiState()
+    object Error : DetailUiState()
+    object Loading : DetailUiState()
+}
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val Mhs: MahasiswaRepository
+) :ViewModel(){
+    var mahasiswaDetailState: DetailUiState by mutableStateOf(DetailUiState.Loading)
+        private set
+
+    private val _nim :String = checkNotNull(savedStateHandle[DestinasiDetail.Nim])
+
+}
